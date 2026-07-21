1a854f1fa00393e508e4043038cfead97bf5d4ae
Fatimah Falah <falahfaty2@gmail.com>
2026-07-21 14:25:51 +0700

    flutter1

---
+++ lib/day5/flutter1.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+
+class Flutter1 extends StatelessWidget {
+  const Flutter1({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xFFF6DDF8),
+      appBar: AppBar(
+        backgroundColor: const Color.fromARGB(255, 243, 33, 33),
+        title: const Text(
+          "Profil Saya",
+          style: TextStyle(color: Colors.white),
+        ),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: const [
+            SizedBox(height: 20),
+
+            // Nama
+            Text(
+              "Fatimah Falah Soebyanto",
+              style: TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+
+            SizedBox(height: 5),
+
+            // Lokasi
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Icon(
+                  Icons.location_on,
+                  color: Color.fromARGB(255, 44, 27, 2),
+                  size: 20,
+                ),
+                SizedBox(width: 5),
+                Text(
+                  "Jakarta, Indonesia",
+                  style: TextStyle(fontSize: 18),
+                ),
+              ],
+            ),
+
+            SizedBox(height: 8),
+
+            // Deskripsi
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 20),
+              child: Text(
+                "Seorang mahasiswa yang sedang belajar Flutter di PPKD Jakarta Pusat",
+                textAlign: TextAlign.left,
+                style: TextStyle(
+                  fontSize: 16,
+                  color: Colors.black54,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
