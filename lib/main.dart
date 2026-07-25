009129ed5e1c59f4028326b42e6b8613cafc8380
Fatimah Falah <falahfaty2@gmail.com>
2026-07-25 17:38:30 +0700

    test commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:ppkd_b7/day8/satck.dart';
-import 'package:ppkd_b7/flutter3.dart';
+import 'package:ppkd_b7/tugas_flutter3.dart';
 
 void main() {
   runApp(const MyApp());
@@ -32,7 +31,7 @@ class MyApp extends StatelessWidget {
         // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const Flutter3(),
+      home: Flutter3(),
     );
   }
 }
